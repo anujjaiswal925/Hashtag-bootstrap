4b331de233f3a474ab2e7be1f44bc224a85fd6de
anujjaiswal925 <63898206+anujjaiswal925@users.noreply.github.com>
2024-12-26 21:07:01 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(".wrap-3carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000, 
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+});
